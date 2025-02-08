45d9a53c11eaa8e6a6840fb08a1ee1360ab4624d
Alan-Cheng <jhcheng.alan@gmail.com>
2025-02-08 21:36:15 +0800

    feat: 完成多人聊天室

--- src/main/java/com/alan/qod/config/WebSocketConfig.java
+++ src/main/java/com/alan/qod/config/WebSocketConfig.java
@@ -11,10 +11,15 @@ import com.alan.qod.service.ChatWebSocketHandler;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(), "/ws")
-                .setAllowedOrigins("*");  // 允許跨域
+        registry.addHandler(chatWebSocketHandler, "/ws/chat").setAllowedOrigins("*");
     }
 }
 
